2cd8efad460c7feb5c04c2b62fcf5987ccda908b
raghav09111 <110532211+raghav09111@users.noreply.github.com>
2022-08-04 11:07:37 +0200

    Update get_data.py

--- get_data.py
+++ get_data.py
@@ -3,7 +3,7 @@
 Available datasets:
 
     * cell: http://www.robots.ox.ac.uk/~vgg/research/counting/index_org.html
-    * shrimp: https://github.com/raghav09111/shrimpdataset.git
+    * shrimp: https://mega.nz/file/V8pRQBLA#_MMNBcO-b6dZJc4wC5OzoATQgGGp50Z3mu0VkGcGdcY
     * ucsd: http://www.svcl.ucsd.edu/projects/peoplecnt/
 """
 import os
@@ -189,7 +189,7 @@ def generate_shrimp_data():
     """Generate HDF5 files for mall dataset."""
     # download and extract dataset
     get_and_unzip(
-        'https://github.com/raghav09111/shrimpdataset'
+        'https://mega.nz/file/V8pRQBLA#_MMNBcO-b6dZJc4wC5OzoATQgGGp50Z3mu0VkGcGdcY'
     )
     # create training and validation HDF5 files
     train_h5, valid_h5 = create_hdf5('shrimp',
